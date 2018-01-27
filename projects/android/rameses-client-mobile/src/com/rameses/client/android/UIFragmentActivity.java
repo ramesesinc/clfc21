1df2386975ec135d142490ceeb0de6201461f841
worgflores <worgflores@gmail.com>
2018-01-27 16:21:45 +0800

    added rameses-client-mobile

---
+++ projects/android/rameses-client-mobile/src/com/rameses/client/android/UIFragmentActivity.java
@@ -0,0 +1,18 @@
+/*
+ * UIActionBarActivity.java
+ *
+ * Created on July 30, 2014, 5:42 PM
+ *
+ * To change this template, choose Tools | Template Manager
+ * and open the template in the editor.
+ */
+
+package com.rameses.client.android;
+
+/**
+ *
+ * @author louie
+ */
+public class UIFragmentActivity extends AbstractFragmentActivity {
+    
+}
